f29eebf7c374e88a03b17afb0e16aecbe3b55d1c
David Stefank <113402850+david-stefanik@users.noreply.github.com>
2023-05-19 23:11:25 -0700

    test

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-require('dotenv').config()
-const express = require('express')
-const { default: mongoose } = require('mongoose')
-const app = express()
-const mongoose = require('mongoose');
+// require('dotenv').config()
+// const express = require('express')
+// const { default: mongoose } = require('mongoose')
+// const app = express()
 
-main().catch(err => console.log(err));
 
-async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:3000/test');}
+
+const post =[{
+    username: 'david',
+   title:'post1' 
+}]
+app.get('/posts',(req,res)=>{
+    res.json(posts)
+})
+
+// main().catch(err => console.log(err));
+
+// async function main() {
+//   await mongoose.connect('mongodb://127.0.0.1:3000/test');}
+  app.listen('')
